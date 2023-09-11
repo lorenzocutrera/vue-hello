1d22a4057a352efde2c643f7db10cf460d8be266
Lorenzo Cutrera <lorenzoq3ra@gmail.com>
2023-09-11 17:12:32 +0200

    add complete es.

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa inserendo il percoso in una propietá dell'oggetto restituito dalla funzione data.
+*/
+
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message : 'Vue Hello',
+            image : 'https://picsum.photos/seed/picsum/200/300'
+        }
+    }
+}).mount('#app')
